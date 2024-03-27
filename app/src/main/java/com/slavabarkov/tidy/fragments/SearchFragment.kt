08d290562c9c2bc0b035d7541d0f6e43c4d8fdd8
YeyeBBC <katlin481@icloud.com>
2024-03-27 13:09:01 +0800

    - Updated dependencies. - Updated Material Design Components. - Added support for Edge-to-edge display (Android 11 or later) - Added smooth keyboard animation (Android 11 or later) - Changed strings from hard coded into referenced, so that they can be localised. - Added Chinese translations. - Added Japanese translations.

--- app/src/main/java/com/slavabarkov/tidy/fragments/SearchFragment.kt
+++ app/src/main/java/com/slavabarkov/tidy/fragments/SearchFragment.kt
@@ -10,6 +10,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsAnimationCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.RecyclerView
@@ -51,6 +54,69 @@ class SearchFragment : Fragment() {
         }
         recyclerView.adapter = ImageAdapter(requireContext(), mSearchViewModel.searchResults!!)
         recyclerView.scrollToPosition(0)
+
+        // If Edge-to-edge display is enabled,
+        // tweak the top margin so that content will not overlap with status bar
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, windowInsets ->
+            // Handle window insets here
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            // Update the layout parameters of the view's parent
+            (v.layoutParams as? ViewGroup.MarginLayoutParams)?.apply {
+                topMargin = insets.top
+            }
+
+            // Use WindowInsets to check the software keyboard visibility.
+            // val imeVisible = windowInsets.isVisible(WindowInsetsCompat.Type.ime())
+            // val imeHeight = windowInsets.getInsets(WindowInsetsCompat.Type.ime()).bottom
+
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
+
+        // New keyboard animation using WindowInsets
+        ViewCompat.setWindowInsetsAnimationCallback(
+            view,
+            object : WindowInsetsAnimationCompat.Callback(DISPATCH_MODE_STOP) {
+
+                var startBottom = 0f
+                override fun onPrepare(
+                    animation: WindowInsetsAnimationCompat
+                ) {
+                    startBottom = view.bottom.toFloat()
+                }
+
+                var endBottom = 0f
+                override fun onStart(
+                    animation: WindowInsetsAnimationCompat,
+                    bounds: WindowInsetsAnimationCompat.BoundsCompat
+                ): WindowInsetsAnimationCompat.BoundsCompat {
+                    // Record the position of the view after the IME transition.
+                    endBottom = view.bottom.toFloat()
+
+                    return bounds
+                }
+
+                override fun onProgress(
+                    insets: WindowInsetsCompat,
+                    runningAnimations: MutableList<WindowInsetsAnimationCompat>
+                ): WindowInsetsCompat {
+                    // Find an IME animation.
+                    val imeAnimation = runningAnimations.find {
+                        it.typeMask and WindowInsetsCompat.Type.ime() != 0
+                    } ?: return insets
+
+                    // Offset the view based on the interpolated fraction of the IME animation.
+                    view.translationY =
+                        (startBottom - endBottom) * (1 - imeAnimation.interpolatedFraction)
+
+                    return insets
+                }
+
+            }
+        )
+
 
         mORTTextViewModel.init()
 
